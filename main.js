baa00e9163db8000aefa3b5f7910fe358273eb78
Francisco <flaurin2009@yahoo.ca>
2021-09-11 21:48:08 -0400

    JSON-HTML-v3
    JSON-HTML-v3, Fetch with WDS.

--- main.js
+++ main.js
@@ -1,1 +1,107 @@
 
+//console.log("Hello Francisco");
+
+//console.log(fetch("https://reqres.in/api/users"));
+
+
+//fetch("https://reqres.in/api/users")
+//.then(res => console.log(res))
+
+
+//fetch("https://reqres.in/api/users")
+//.then(res => res.json())
+//.then(data => console.log(data))
+
+
+
+
+/*
+fetch("https://reqres.in/api/users/33")
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(error => console.log("totalmente erroneo"))
+*/
+
+
+
+/*
+fetch("https://reqres.in/api/users/33")
+.then(res => {
+ if (res.ok) {
+  console.log("success");
+} else {
+   console.log("error");
+}
+
+})
+.then(data => console.log(data))
+.catch(error => console.log("totalmente erroneo"))
+*/
+
+
+/*
+fetch("https://reqres.in/api/users/3")
+.then(res => {
+ if (res.ok) {
+  console.log("success");
+} else {
+   console.log("error");
+}
+
+})
+*/
+
+
+//basics Object manipulation from a fake API (JSON object) adding a new user and provide an ID
+
+
+fetch("https://reqres.in/api/users/", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+name: "Francisco", id: 547
+})
+
+
+}).then(res => {
+return res.json() 
+})
+.then(data => console.log(data))
+.catch(error => console.log("totalmente erroneo"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
